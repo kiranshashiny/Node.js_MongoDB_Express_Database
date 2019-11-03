2c740f626eb9f12ade2502c91bee1c1ec0828be6
Shashi Kiran <kiranshashiny@gmail.com>
2019-11-03 18:08:24 +0530

    Updated

--- findall.js
+++ findall.js
@@ -4,37 +4,65 @@ var url = "mongodb://localhost:27017/";
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   var dbo = db.db("mydb");
-/*  dbo.collection("powers").find().toArray( function(err, result) {
+  
+/*
+ dbo.collection("stocks_summary").find().toArray( function(err, result) {
     if (err) throw err;
     console.log(result);
     db.close();
   });
 */
- 
-  console.log ( "shashi"); 
 
+  // find just one. GTX, ATEN
+  const cursor = dbo.collection('stocks_summary').find({ Symbol: "WUBA" }).toArray( function ( err, result ) {
+     console.log ( result );
+     db.close();
+  });
+
+
+/*
   // This one gets me just the records that have the word flying.
   const cursor = dbo.collection('powers').find({ name: "flying" }).toArray( function ( err, result ) {
      console.log ( result );
      db.close();
   });
 
+  const cursor = dbo.collection('powers').find({ name: /^[a-z]/ }).toArray( function ( err, result ) {
+     console.log ( result );
+     //db.close();
+  });
 
-  /*
-  console.log ("Now printing the number of elements 'stocks'")
-  var query = { stock:"GTX"};
-  dbo.collection("stocks").find({query}).toArray( function(err, result) {
+
+  dbo.collection("stocks").find().toArray( function(err, result) {
+    if (err) throw err;
+    console.log(result);
+    //db.close();
+  });
+*/
+/*
+  dbo.collection("stocks_summary").find({}).toArray( function(err, result) {
     if (err) throw err;
 
     console.log(result);
     db.close();
   });
-  */
+*/
+/* 
+  console.log ("Now printing the number of elements 'stocks_summary'")
+  var query = { displayName:'GTX'};
+  dbo.collection("stocks_summary").find({query}).toArray( function(err, result) {
+    if (err) throw err;
+
+    console.log(result);
+    db.close();
+  });
+*/ 
 /*  dbo.collection("myheroes").find({}).toArray( function(err, result) {
     if (err) throw err;
 
     console.log(result);
     db.close();
-  });*/
+  });
+*/
 
 });
